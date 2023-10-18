5d5121d1996e229fa1fa9766e679a7c2439d4d2f
Skisocks <jaa.skeoch@gmail.com>
2023-10-18 14:41:18 +0100

    chore: satisfy linter

--- pkg/credhelper/helper.go
+++ pkg/credhelper/helper.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -58,10 +58,7 @@ func isACRRegistry(input string) bool {
 		return true
 	}
 	matches := acrRE.FindStringSubmatch(serverURL.Hostname())
-	if len(matches) == 0 {
-		return false
-	}
-	return true
+	return len(matches) != 0
 }
 
 func (a ACRCredHelper) Get(serverURL string) (string, string, error) {
